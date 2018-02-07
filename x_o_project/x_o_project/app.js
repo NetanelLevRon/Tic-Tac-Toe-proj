3f154bf9f992d9b8c73c8d8de3d823b7b09a8390
Netanel Lev-Ron <NetanelLevRonfg@gmail.com>
2018-02-08 01:24:34 +0200

    second step-enter to all Arrays x in there places for chacking victory or where to put o

--- x_o_project/x_o_project/app.js
+++ x_o_project/x_o_project/app.js
@@ -5,6 +5,11 @@ var x_oStrDoc = "";
 var x_oMove;
 var mainDiagonal = x_oMat.length + x_oMat.length;
 var secondDiagonal = x_oMat.length + x_oMat.length + 1;
+var victoryOptionsArr = new Array(8);
+for (var i = 0; i < victoryOptionsArr.length; i++) {
+    victoryOptionsArr[i] = new VictoryOptions();
+    victoryOptionsArr[i].victoryArr = new Array(3);
+}
 for (var i = 0; i < x_oMat.length; i++) {
     x_oMat[i] = new Array(3);
     for (var j = 0; j < x_oMat[i].length; j++, counter++) {
@@ -26,7 +31,20 @@ do {
         if (x_oMove > 0 && x_oMove < 10) {
             for (var j = 0; j < x_oMat[i].length; j++, counter_1++) {
                 if (x_oMove == counter_1) {
+                    if (x_oMat[i][j] == "x" || x_oMat[i][j] == "o") {
+                        do {
+                            x_oMove = Number(prompt(x_oStrPrompt_1 + '\nThis place olready taken!\nPlease enter "x" in other place.'));
+                        } while (x_oMat[i][j] == "x" || x_oMat[i][j] == "o");
+                    }
                     x_oMat[i][j] = "x";
+                    victoryOptionsArr[i].victoryArr[j] = "x";
+                    victoryOptionsArr[j].victoryArr[i] = "x";
+                    if (i == j) {
+                        victoryOptionsArr[mainDiagonal].victoryArr[i] = "x";
+                    }
+                    if (i + j == (x_oMat[i].length) - 1) {
+                        victoryOptionsArr[secondDiagonal].victoryArr[i] = "x";
+                    }
                 }
                 x_oStrPrompt_1 += "|    " + x_oMat[i][j] + "    ";
                 x_oStrDoc += "|&nbsp&nbsp" + x_oMat[i][j] + "&nbsp&nbsp";
@@ -37,10 +55,5 @@ do {
     }
 } while (x_oMove <= 0 || x_oMove >= 10);
 document.write(x_oStrDoc + "");
-var victoryOptionsArr = new Array(8);
-for (var i = 0; i < victoryOptionsArr.length; i++) {
-    victoryOptionsArr[i] = new VictoryOptions();
-    victoryOptionsArr[i].victoryArr = new Array(3);
-}
 victoryOptionsArr[0].victoryArr[0] = "x";
 //# sourceMappingURL=app.js.map
--- x_o_project/x_o_project/app.ts
+++ x_o_project/x_o_project/app.ts
@@ -13,6 +13,15 @@ let mainDiagonal: number = x_oMat.length + x_oMat.length;
 
 let secondDiagonal: number = x_oMat.length + x_oMat.length + 1;
 
+let victoryOptionsArr: Array<VictoryOptions> = new Array<VictoryOptions>(8);
+
+for (let i: number = 0; i < victoryOptionsArr.length; i++) {
+
+    victoryOptionsArr[i] = new VictoryOptions();
+    victoryOptionsArr[i].victoryArr = new Array<string>(3)
+}
+
+
 for (let i: number = 0; i < x_oMat.length; i++) {
     x_oMat[i] = new Array<string>(3);
     for (let j: number = 0; j < x_oMat[i].length; j++ , counter++) {
@@ -42,7 +51,27 @@ do {
             for (let j: number = 0; j < x_oMat[i].length; j++ , counter++) {
 
                 if (x_oMove == counter) {
-                    x_oMat[i][j] = "x";
+
+                    if (x_oMat[i][j] == "x" || x_oMat[i][j] == "o") {
+                        do {
+                            x_oMove = Number(prompt(x_oStrPrompt + '\nThis place olready taken!\nPlease enter "x" in other place.'));
+                        } while (x_oMat[i][j] == "x" || x_oMat[i][j] == "o");
+                    }
+
+                        x_oMat[i][j] = "x";
+
+                        victoryOptionsArr[i].victoryArr[j] = "x";
+                        victoryOptionsArr[j].victoryArr[i] = "x";
+                        if (i == j) {
+                            victoryOptionsArr[mainDiagonal].victoryArr[i] = "x";
+                        }
+                        if (i + j == (x_oMat[i].length) - 1) {
+                            victoryOptionsArr[secondDiagonal].victoryArr[i] = "x";
+                        }
+
+
+                
+
                 }
 
                 x_oStrPrompt += "|    " + x_oMat[i][j] + "    ";
@@ -57,13 +86,7 @@ do {
 
 document.write(x_oStrDoc + "");
 
-let victoryOptionsArr: Array<VictoryOptions> = new Array<VictoryOptions>(8);
 
-for (let i: number = 0; i < victoryOptionsArr.length; i++) {
-
-    victoryOptionsArr[i] = new VictoryOptions();
-    victoryOptionsArr[i].victoryArr = new Array<string>(3)
-}
 
 victoryOptionsArr[0].victoryArr[0] = "x";
 
